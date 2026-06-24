f0fb2a801fc883c6c8e6c52ede03f64a1e33295a
Nandini14056 <nandiniraulji1456@gmail.com>
2026-06-24 17:17:29 +0530

    added authentication middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User } = require("../model/user.model");
+
+const verifyJwt = async (req, res, next) => {
+  try {
+    const authHeder = req.header("Authorization");
+
+    if (!authHeder) {
+      return res.status(400).json({
+        message: "token not found"
+      })
+    }
+
+    const token = authHeder.split(" ")[1];
+
+    const decode = await jwt.verify(token, process.env.SECRET_KEY);
+
+    const user = await User.findById(decoded._id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      })
+    }
+    req.user = user;
+
+    next();
+
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    })
+  }
+}
+
+module.exports = {
+  verifyJwt
+}
